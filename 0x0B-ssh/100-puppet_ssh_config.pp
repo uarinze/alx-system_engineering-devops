f7110d43da4e5f4b572498bd301b6e105d5878c7
Ugo <alexarinzeugo@yahoo.com>
2024-03-28 00:19:40 +0000

    added file 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
 file { '~./ssh/config/':
-  ensure => 'present',
-  content => "PasswordAuthentication no\nIdentityFile ~/.ssh/school",
+	ensure => 'present',
+	content => "PasswordAuthentication no\nIdentityFile ~/.ssh/school",
 }
