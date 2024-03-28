7e0afd07c92e21ba9cf87884ccda826b159c7cca
Ugo <alexarinzeugo@yahoo.com>
2024-03-28 01:45:31 +0000

    edit

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 file {'config':
-  path		=> /home/.ssh/config,
+  path		=> '/home/vagrant/.ssh/config',
   ensure	=> present,
   content	=> "Host *\n\tPasswordAuthentication no\n\tIdentityFile ~/.ssh/school",
 }
